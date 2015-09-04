68b6ef3a34c0a44873396e5dea6ccebbea82dc66
Otto Lin <ottolin@gmail.com>
2015-09-04 23:53:23 +0800

    mix.exs: adding Telebot application mod

--- mix.exs
+++ mix.exs
@@ -14,7 +14,10 @@ defmodule Telebot.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger, :httpoison]]
+    [
+    applications: [:logger, :httpoison],
+    mod: {Telebot, []}
+    ]
   end
 
   # Dependencies can be Hex packages:
