85e5a7c285edb9bfd163ea5269aa74c1e3c4e79e
Otto Lin <ottolin@gmail.com>
2015-09-05 16:26:57 +0800

    echo: using Telegram.Api directly in reply

--- lib/handlers/echo.ex
+++ lib/handlers/echo.ex
@@ -4,18 +4,19 @@ defmodule Echo do
 
   def text(chat, t) do
     Logger.debug "Echo: recv text: " <> t
-    resp chat, "Echo: " <> t
+    Telebot.Api.send_message chat.id, "Echo: " <> t
   end
 
   def contact(chat, c) do
     {:ok, t} = Poison.encode(c)
     Logger.debug "Echo: recv contact: " <> t
+    Telebot.Api.send_message chat.id, "Echo: " <> c.first_name <> " tel: " <> c.phone_number
   end
 
   def location(chat, l) do
     {:ok, t} = Poison.encode(l)
     Logger.debug "Echo: recv location: " <> t
+    Telebot.Api.send_location chat.id, l.latitude, l.longitude
   end
 
-  #TODO
 end
